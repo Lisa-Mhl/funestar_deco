5d00a5a5779a991b72651998421a72b65ba576c4
lilou <michellisa.75@gmail.com>
2020-09-06 12:20:25 +0200

    add category & articles details

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,4 +20,28 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    /**
+     * @Route("/articles", name="articles")
+     * @param ArticleRepository $articleRepository
+     * @return Response
+     */
+    public function articles(ArticleRepository $articleRepository)
+    {
+        return $this->render('home/articles.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/articles/{id}", name="articles_details")
+     */
+    public function detailsArticles(Article $article)
+    {
+        return $this->render('home/articles_details.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -42,6 +42,17 @@ class Article
      */
     private $created_at;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -103,6 +114,30 @@ class Article
     public function setCreatedAt(?\DateTimeInterface $created_at): self
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
